51f2bf8766bb87f0fccb0d0e070503b976bd8ab0
aswin <aswin.cshajikumar@gmail.com>
2024-04-29 13:29:28 +0530

    tailwind

---
+++ new-project/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <main>
+      <div className="flex items-center justify-center  h-screen w-screen  bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90">
+
+
+        <button type="button" class=" h-11 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500  ">
+          Hover me
+        </button>
+
+
+      </div>
+    </main>
+  )
+}
+
+export default App
